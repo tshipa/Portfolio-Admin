e26946c0901a193217545f74cc2f69c39183ab7c
tshipa <tshipa.madi@gmail.com>
2013-06-27 10:55:36 +0200

    hello world pop up

--- Portfolio-Admin System/Form1.cs
+++ Portfolio-Admin System/Form1.cs
@@ -15,5 +15,10 @@ namespace Portfolio_Admin_System
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
     }
 }
